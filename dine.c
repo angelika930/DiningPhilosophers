0a82c6ff64a4340a9680526ceec9be4d181c9ade
Angelika Canete <acanete@calpoly.edu>
2025-02-02 15:52:57 -0800

    draft of child function

--- dine.c
+++ dine.c
@@ -6,12 +6,16 @@
 #endif
 #define STRINGSIZE 15
 
+
+//Create global semaphore for printing
+sem_t semPrint;
 //Initiate struct philosopher data
 typedef struct philosopher_info {
    sem_t leftFork;
    sem_t rightFork;
    char status[STRINGSIZE];
    pthread_t *threadPtr;
+   bool isEven = 0; //used to specify if phil is even or odd numbered
 } Philosopher;
 
 //print out status of philosophers
@@ -21,26 +25,61 @@ void printStatus(Philosopher *listPhil[]) {
    }
    printf("\n");
 }
-
 //Initiate list of semaphores
 sem_t semaphores[NUM_PHILOSOPHERS];
 //Initiate list of philosopher structs
 Philosopher *listPhil[NUM_PHILOSOPHERS];
-void philAction(Philosopher *child) {}
+
+void philAction(Philosopher *child) {
+   if (child->isEven == 0) { //Pick up right fork first
+      sem_wait(&child->rightFork);
+
+      sem_wait(&semPrint);
+      //printf FIGURE OUT HOW TO CHANGE NUMBERS IN PRINT STATUS
+      sem_post(&semPrint);
+
+      sem_wait(&child->leftFork);
+      
+      sem_wait(&semPrint);
+      //printf for picking up left fork
+      sem_post(&semPrint);
+
+      dawdle(); //eat for random amount
+      sem_post(&child->rightFork); //set down right fork
+   
+      sem_wait(&semPrint);
+      //PRINT AGAIN STATUS HERE of putting down right fork
+      sem_post(&semPrint);
+
+      sem_post(&child->leftFork); //set down left fork
+      
+      sem_wait(&semPrint);
+      //PRINT AGAIN STATUS HERE for putting down left fork
+      //print new line
+      //print -------
+      //print think line
+      sem_post(&semPrint);
+
+      dawdle(); //dawdle for think
+
+      
+      sem_wait(&semPrint);
+      //PRINT AGAIN STATUS HEre for -----
+      sem_post(&semPrint);
+      
+
+      
+      
+
+   }
 
 
-
-
-
-
-
-
-
+}
 
 int main() {
 
-
-
+//initialize sempahore for printing status
+sem_init(&semPrint, 0, 1); 
 //Initiate semaphore list, each fork is rep by semaphore
 for (int j = 0; j < NUM_PHILOSOPHERS; j++) {
    sem_init(&semaphores[j], 0, 1); 
@@ -48,7 +87,7 @@ for (int j = 0; j < NUM_PHILOSOPHERS; j++) {
 
 //Allocate memory for philosopher structs
 for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-
+   //malloc for new philosopher struct 
    Philosopher newPhilosopher = malloc(sizeof(Philosopher));
    if (newPhilosopher == NULL) {
       perror("Malloc failed to create philosopher struct");
@@ -61,6 +100,14 @@ for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
       perror("Failed to create a new thread");
       exit(1);
    }
+   //check if phil is even or odd to determine which fork to pick up first
+   //avoids deadlocking
+   if (i%2 == 0) {
+      isEven = 1;
+   }
+   else {
+      isEven = 0;
+   }
    //specifies which semaphores are the left and right forks
    if (i == 0) {//special right fork for first philosophers
       leftFork = semaphores[0];
@@ -70,7 +117,6 @@ for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
       leftFork = semaphores[i];
       rightFork = semaphores[i-1];
    }
-
 }
 
 
